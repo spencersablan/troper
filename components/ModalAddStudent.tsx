46c16ea44d7fb1e036a7de105525af50e06461d1
Spencer Sablan <spencertsablan@gmail.com>
2022-06-22 22:30:16 -0700

    Add student functionality

--- components/ModalAddStudent.tsx
+++ components/ModalAddStudent.tsx
@@ -10,43 +10,59 @@ import mainStyles from "../styles/modules/modal-add-student.module.scss";
 import btnStyles from "../styles/components/button.module.scss";
 import inputStyles from "../styles/components/input.module.scss";
 import toggleStyles from "../styles/components/toggle.module.scss";
-import { useRef } from "react";
+import { useState, useRef } from "react";
 
 export default function ModalAddStudent(props) {
+	// Handle gender toggler
+	const [checked, setChecked] = useState(false);
+
 	// User
 	const { user, loading, error } = useAuth();
 
 	// Add Student
+	const firstName = useRef(null as HTMLInputElement | null);
+	const lastName = useRef(null as HTMLInputElement | null);
+	const gender = useRef(null as HTMLInputElement | null);
+
+	// TODO: Add error handling
 	const addStudent = async () => {
+		const genderValue = gender.current.checked ? "Male" : "Female";
 		const newStudentID = uuidv4();
 		const studentRef = doc(firestore, "students", newStudentID);
+
 		await setDoc(studentRef, {
+			firstName: firstName.current.value,
+			lastName: lastName.current.value,
+			gender: genderValue,
 			teacherID: user.uid,
-			firstName: "Keenan",
-			lastName: "Allen",
 			studentID: newStudentID,
 		});
-	};
 
-	// Handle gender toggler
-	const checkbox = useRef(null);
+		props.close();
+	};
 
 	return (
 		<Modal>
 			<ModalHeader>Add Student</ModalHeader>
+
 			<ModalBody>
 				<div className={mainStyles.body}>
-					<input className={inputStyles.input} placeholder="First Name" />
-					<input className={inputStyles.input} placeholder="Last Name" />
+					<input ref={firstName} className={inputStyles.input} placeholder="First Name" />
+					<input ref={lastName} className={inputStyles.input} placeholder="Last Name" />
 					<label className={toggleStyles.toggle}>
-						<input type="checkbox" className={toggleStyles.checkbox} />
-						<div className={`${toggleStyles.option} ${toggleStyles.optionA}`}>Male</div>
-						<div className={`${toggleStyles.option} ${toggleStyles.optionB}`}>Female</div>
+						<input ref={gender} type="checkbox" className={toggleStyles.checkbox} checked={checked} />
+						<div className={`${toggleStyles.option} ${toggleStyles.optionA}`} onClick={() => setChecked(true)}>
+							Male
+						</div>
+						<div className={`${toggleStyles.option} ${toggleStyles.optionB}`} onClick={() => setChecked(false)}>
+							Female
+						</div>
 					</label>
 				</div>
 			</ModalBody>
+
 			<ModalFooter>
-				<button onClick={props.close} className={btnStyles.btn}>
+				<button onClick={addStudent} className={btnStyles.btn}>
 					Create Student
 				</button>
 			</ModalFooter>
