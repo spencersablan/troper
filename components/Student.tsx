e641348f4db318edbfe638aba3dcd85b1f16e2f3
Spencer Sablan <spencertsablan@gmail.com>
2022-07-02 11:19:10 -0700

    Student page top

--- components/Student.tsx
+++ components/Student.tsx
@@ -1,14 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import SvgChevron from "./SvgChevron";
+import SvgSettings from "./SvgSettings";
 import styles from "../styles/modules/student.module.scss";
+import btnStyles from "../styles/components/button.module.scss";
+import tabsStyles from "../styles/components/tabs.module.scss";
+
+enum Quarters {
+	Q1,
+	Q2,
+	Q3,
+	Q4,
+}
 
 export default function Student({ student, back }) {
+	const [activeQuarter, setActiveQuarter] = useState(Quarters.Q1);
+
 	return (
-		<div>
-			<a className={styles.back} onClick={back}>
-				<SvgChevron className={styles.backChevron} />
-				<span className={styles.backText}>Back to Home</span>
-			</a>
+		<div className={styles.student}>
+			<div className={styles.topActions}>
+				<a
+					className={`${btnStyles.iconTextBtn} ${btnStyles.modIconOnlyMobile} ${btnStyles.modAlignLeft}`}
+					onClick={back}
+				>
+					<SvgChevron className={`${btnStyles.btnIcon} ${btnStyles.backIcon}`} />
+					<span className={btnStyles.btnText}>Back to Home</span>
+				</a>
+
+				{/* TODO: Create Student Settings Modal */}
+				<a className={btnStyles.iconTextBtn} onClick={back}>
+					<SvgSettings className={`${btnStyles.btnIcon} ${btnStyles.settingsIcon}`} />
+					<span className={btnStyles.btnText}>Edit Student</span>
+				</a>
+			</div>
+			<div className={styles.studentFormHeader}>
+				<div className={tabsStyles.tabsContainer}>
+					<div
+						className={`${tabsStyles.tab} ${activeQuarter === Quarters.Q1 && tabsStyles.active}`}
+						onClick={() => setActiveQuarter(Quarters.Q1)}
+					>
+						Q1
+					</div>
+					<div
+						className={`${tabsStyles.tab} ${activeQuarter === Quarters.Q2 && tabsStyles.active}`}
+						onClick={() => setActiveQuarter(Quarters.Q2)}
+					>
+						Q2
+					</div>
+					<div
+						className={`${tabsStyles.tab} ${activeQuarter === Quarters.Q3 && tabsStyles.active}`}
+						onClick={() => setActiveQuarter(Quarters.Q3)}
+					>
+						Q3
+					</div>
+					<div
+						className={`${tabsStyles.tab} ${activeQuarter === Quarters.Q4 && tabsStyles.active}`}
+						onClick={() => setActiveQuarter(Quarters.Q4)}
+					>
+						Q4
+					</div>
+				</div>
+				<h2 className={styles.studentName}>
+					{student.firstName} {student.lastName}
+				</h2>
+			</div>
 		</div>
 	);
 }
